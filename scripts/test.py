7054140f5c54889b21e3a303d0e94f0f558417be
ethantqiu <ethantqiu@berkeley.edu>
2023-11-16 19:39:05 -0800

    ur fix

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,9 @@
 from ur5py.ur5 import UR5Robot
 import numpy as np
 
-if __name__=='__main__':
+if __name__ == "__main__":
     ur = UR5Robot()
-    input('enter to enter freedrive')
-    ur.start_freedrive()
-    input('enter tro end freedrive')
-    ur.stop_freedrive()
+    input("enter to enter freedrive")
+    ur.start_teach()
+    input("enter tro end freedrive")
+    ur.stop_teach()
