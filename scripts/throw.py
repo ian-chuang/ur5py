b3a6dd0be6cb8dc07189d8a293b22baa6757920a
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-12-05 19:29:01 -0800

    prelim throw

--- scripts/throw.py
+++ scripts/throw.py
@@ -7,10 +7,11 @@ robot = UR5Robot(ip="192.168.131.69", gripper=2)
 current_pose = np.array(robot.get_pose(convert=False))
 
 end_pose = current_pose.copy()
-end_pose[2] -= 0.01
+end_pose[1] += 0.2
 poses = np.linspace(current_pose, end_pose, 1000)
 pdb.set_trace()
-for row in poses:
+for i, row in enumerate(poses):
     start_time = time.time()
-    robot.servo_pose(row, time=0.02, convert=False)
-    time.sleep(0.02)
+    robot.servo_pose(row, time=0.004, convert=False)
+    if time.time - start_time < 0.004:
+        pass
