69e3a690beaa2c698ed1e8a249615c97fb4f7e04
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-12-05 19:57:27 -0800

    basic code structure

--- scripts/throw.py
+++ scripts/throw.py
@@ -7,13 +7,15 @@ robot = UR5Robot(ip="192.168.131.69", gripper=2)
 current_pose = np.array(robot.get_pose(convert=False))
 
 end_pose = current_pose.copy()
-end_pose[1] += 0.2
-poses = np.linspace(current_pose, end_pose, 1000)
+end_pose[1] -= 0.4
+poses = np.linspace(current_pose, end_pose, 250)
 pdb.set_trace()
 for i, row in enumerate(poses):
+    print(i)
     start_time = time.time()
-    robot.servo_pose(row, time=0.004, convert=False)
-    if i == 500:
+    robot.servo_pose(row, time=0.002, convert=False)
+    if i == 100:
         robot.gripper.open()
-    if time.time - start_time < 0.004:
+    while time.time() - start_time < 0.002:
         pass
+robot.stop_joint(3)
