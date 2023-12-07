b47207a9a684846a62fc8d7593ad76a474811b28
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-12-06 20:56:49 -0800

    integration testing

--- scripts/find_waypoints.py
+++ scripts/find_waypoints.py
@@ -6,6 +6,71 @@ from roboticstoolbox.tools.trajectory import quintic
 from roboticstoolbox.tools.trajectory import trapezoidal
 
 from autolab_core import RigidTransform
+import numpy as np
+
+from ur5py.ur5 import UR5Robot, RT2UR, UR2RT
+import time
+import pdb
+import roboticstoolbox
+from autolab_core import RigidTransform
+from roboticstoolbox.tools.trajectory import (
+    quintic_func,
+    quintic,
+    trapezoidal,
+    trapezoidal_func,
+)
+
+
+def max_v(ini_pos, target_pos, t):
+    f = trapezoidal_func(ini_pos, target_pos, t)
+    _, max_v, _ = f(t / 2)
+    return max_v
+
+
+def time_from_v(ini_pos, target_pos, target_v, time_per_step):
+    virtual_target = (target_pos) + (target_pos - ini_pos)
+    dist = np.linalg.norm(virtual_target - ini_pos)
+    target_v_norm = np.linalg.norm(target_v)
+    min = dist / target_v_norm
+    max = min * 2
+    target_t = -1
+    for t in np.arange(min, max, step=0.001):
+        velocity = np.zeros(3)
+        for i in range(3):
+            velocity[i] = max_v(ini_pos[i], virtual_target[i], t)
+        if np.linalg.norm(velocity - target_v) <= 0.05:
+            target_t = t
+            break
+    num_of_points = int(target_t / time_per_step)
+
+    final_traj = np.zeros((num_of_points, 3))
+    for i in range(3):
+        trajectory = trapezoidal(ini_pos[i], virtual_target[i], num_of_points)
+        print(num_of_points)
+        positions = trajectory.q
+        print(positions)
+        final_traj[:, i] = positions
+    return final_traj
+
+
+def do_throw(robot: UR5Robot, end_pose, instant_vel):
+    robot.move_pose(end_pose)
+    instant_vel = np.array(instant_vel)
+    intermediate_pose = UR2RT(end_pose)
+    intermediate_pose[:3] -= instant_vel / np.linalg.norm(instant_vel) * 0.3
+    robot.move_pose(intermediate_pose, convert=False)
+    poses = time_from_v(intermediate_pose, end_pose, instant_vel, 0.002)
+    release = int(len(poses) // 2)
+    for i, p in enumerate(poses):
+        curr_time = time.time()
+        robot.servo_pose(p, time=0.002, convert=False)
+        if i == release:
+            robot.gripper.open()
+        while time.time() - curr_time < 0.002:
+            pass
+        if i > release + 300:
+            break
+    robot.stop_joint(5)
 
 
 def find_waypoints(current_pose, start, goal, total_t):
@@ -83,26 +148,12 @@ current_pose = np.array(robot.get_pose(convert=False))
 end_pose = current_pose.copy()
 end_pose[1] -= 0.4
 start = np.array([-0.6, 0.2, 0])
-goal = np.array([0, -3, 0])
-total_time = 1
+goal = np.array([0, -1, 0])
+total_time = 0.5
 poses_temp, m, v_i = find_waypoints(current_pose, start, goal, total_time)
 # transform m to end pose
 tsfm = RigidTransform(m[:3, :3], m[:3, 3])
-robot.move_pose(tsfm)
-tsfm.translation += v_i / np.linalg.norm(v_i) * 0.2
-robot.move_pose(tsfm)
-# poses = find_waypoints(current_pose, start, goal, total_time)
-poses = np.linspace(current_pose, end_pose, 250)
-pdb.set_trace()
-for i, row in enumerate(poses):
-    print(i)
-    start_time = time.time()
-    robot.servo_pose(row, time=0.04, convert=False)
-    # if i == 100:
-    #     robot.gripper.open()
-    while time.time() - start_time < 0.04:
-        pass
-robot.stop_joint(3)
+do_throw(robot, tsfm, v_i)
 
 
 def helper_sine(ini_start, ini_end, num):
